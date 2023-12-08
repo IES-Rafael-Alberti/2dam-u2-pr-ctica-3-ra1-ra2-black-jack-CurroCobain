448d7aeddeb58f6273a215653da62c14667dda82
CurroCobain <123578572+CurroCobain@users.noreply.github.com>
2023-12-08 13:20:10 +0100

    Delete app/src/main/java/com/fmunmar310/newblackjack/clases directory

--- app/src/main/java/com/fmunmar310/newblackjack/clases/Palos.kt
+++
@@ -1,8 +0,0 @@
-package com.fmunmar310.newblackjack.clases
-
-/**
- * Clase tipo enum que indica los tipos de palos de la baraja
- */
-enum class Palos (val valor : Int){
-    CORAZONES (0), DIAMANTES (1), TREBOLES (2), PICAS (3)
-}
